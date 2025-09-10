74946a169ac224b7316844135abd50b0a1729768
Leo Jovanov <leojovanov22@gmail.com>
2025-09-10 13:17:16 -0500

    Added modifiers to add list title and change font type and color.

--- ContentView.swift
+++ ContentView.swift
@@ -3,10 +3,17 @@ import SwiftUI
 struct ContentView: View {
     @State var tasks: [Task] = [Task(nameOfTask:"Mow the Lawn"), Task(nameOfTask:"Feed the Pets"), Task(nameOfTask:"Do the Dishes")]
     var body: some View {
+        Text("List Keeper")
+            .font(.largeTitle)
+            .foregroundStyle(.blue)
+            .shadow(radius: 10)
         List(tasks, id: \.self) { task in
             Text(task.nameOfTask)
         }
         .padding()
+        .foregroundStyle(.red)
+        .font(.custom("Chalkduster", size: 30))
+        .shadow(radius: 10)
     }
 }
 
