d477606a4ae0a1a7a80d4930b421a8e239c13638
Aadi Bilfaqi <abilfaqi7040@stu.d214.org>
2025-09-10 13:14:38 -0500

    I changed the data type to Task. I applied the Task initializer to the strings. I also added .nameOfTask to teh text view value.

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tasks: [String] = ["Mow the Lawn", "Feed the Pets", "Do the Dishes"]
+    @State var tasks: [Task] = [Task(nameOfTask:"Mow the Lawn"), Task(nameOfTask:"Feed the Pets"), Task(nameOfTask:"Do the Dishes")]
     var body: some View {
         List(tasks, id: \.self) { task in
-            Text(task)
+            Text(task.nameOfTask)
         }
         .padding()
     }
