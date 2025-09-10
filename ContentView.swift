a0979fad58ad4e1508f459f7d8b28e7f1a195952
Aadi Bilfaqi <abilfaqi7040@stu.d214.org>
2025-09-10 13:21:38 -0500

    Made a Button to clear the tasks. I also added header view to the top and passed it through the tasks array.

--- ContentView.swift
+++ ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @State var tasks: [Task] = [Task(nameOfTask:"Mow the Lawn"), Task(nameOfTask:"Feed the Pets"), Task(nameOfTask:"Do the Dishes")]
     var body: some View {
+        HeaderView(tasks: $tasks)
         Text("List Keeper")
             .font(.largeTitle)
             .foregroundStyle(.blue)
@@ -14,6 +15,9 @@ struct ContentView: View {
         .foregroundStyle(.red)
         .font(.custom("Chalkduster", size: 30))
         .shadow(radius: 10)
+        Button("Clear All Tasks") {
+            tasks.removeAll()
+        }
     }
 }
 
